6b8ccad3e03f29cc0350ea6e7cf48c4b2f269b94
Cooper <cooper.benson@outofsyncstudios.com>
2021-09-06 15:33:39 -0600

    fix: Fixing linting and formatting rules

--- src/util/jsonpointer.test.ts
+++ src/util/jsonpointer.test.ts
@@ -1,34 +1,36 @@
-import {expect} from 'chai';
-import {parse, unescapeToken} from './jsonpointer';
+import { expect } from 'chai';
+import { parse, unescapeToken } from './jsonpointer';
 
-describe('jsonpointer.unescapeToken', () => {
-  it('unescapes ~1 to /', () => {
-    expect(unescapeToken('foo~1bar')).to.equal('foo/bar');
+describe('jsonpointer', () => {
+  describe('unescapeToken', () => {
+    it('unescapes ~1 to /', () => {
+      expect(unescapeToken('foo~1bar')).to.equal('foo/bar');
+    });
+
+    it('unescapes ~0 to ~', () => {
+      expect(unescapeToken('foo~0bar')).to.equal('foo~bar');
+    });
+
+    it('unescapes a mix of ~0 and ~1', () => {
+      expect(unescapeToken('foo~0~1bar')).to.equal('foo~/bar');
+    });
   });
 
-  it('unescapes ~0 to ~', () => {
-    expect(unescapeToken('foo~0bar')).to.equal('foo~bar');
-  });
+  describe('parse', () => {
+    it('throws on an empty pointer', () => {
+      expect(() => parse('')).to.throw();
+    });
 
-  it('unescapes a mix of ~0 and ~1', () => {
-    expect(unescapeToken('foo~0~1bar')).to.equal('foo~/bar');
-  });
-});
+    it('throws on an invalid pointer', () => {
+      expect(() => parse('foo/bar')).to.throw();
+    });
 
-describe('jsonpointer.parse', () => {
-  it('throws on an empty pointer', () => {
-    expect(() => parse('')).to.throw();
-  });
+    it('parses a simple JSON pointer', () => {
+      expect(parse('/foo/bar')).to.have.ordered.members(['foo', 'bar']);
+    });
 
-  it('throws on an invalid pointer', () => {
-    expect(() => parse('foo/bar')).to.throw();
+    it('parses an escaped JSON pointer', () => {
+      expect(parse('/foo~0/bar~1baz')).to.have.ordered.members(['foo~', 'bar/baz']);
+    });
   });
-
-  it('parses a simple JSON pointer', () => {
-    expect(parse('/foo/bar')).to.have.ordered.members(['foo', 'bar']);
-  });
-
-  it('parses an escaped JSON pointer', () => {
-    expect(parse('/foo~0/bar~1baz')).to.have.ordered.members(['foo~', 'bar/baz']);
-  })
 });
