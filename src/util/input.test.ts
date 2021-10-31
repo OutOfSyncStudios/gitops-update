722076c27e0f4fe11096be6bc94732b0eaa00fa9
Cooper <cooper.benson@outofsyncstudios.com>
2021-10-31 14:44:18 -0600

    test: Adding coverage for the YAML input

--- src/util/input.test.ts
+++ src/util/input.test.ts
@@ -2,11 +2,12 @@ import { InputOptions } from '@actions/core';
 import { expect } from 'chai';
 import proxyquire from 'proxyquire';
 import { stub, SinonStub } from 'sinon';
-import { getIntegerInput } from './input';
+import { getIntegerInput, getYamlInput } from './input';
 
 let getInputStub: SinonStub<[string, InputOptions?], string>;
 let stubbedInputModule: {
   getIntegerInput: typeof getIntegerInput;
+  getYamlInput: typeof getYamlInput;
 };
 
 describe('Input utilities', () => {
@@ -27,6 +28,15 @@ describe('Input utilities', () => {
       const key = 'input';
       getInputStub.returns('foobar');
       expect(() => stubbedInputModule.getIntegerInput(key)).to.throw(TypeError);
+      getInputStub.calledOnceWith(key);
+    });
+  });
+
+  describe('getYamlInput', () => {
+    it('correctly parses a yaml string', () => {
+      const key = 'input';
+      getInputStub.returns('foo: bar');
+      expect(stubbedInputModule.getYamlInput(key)).to.have.property('foo', 'bar');
       getInputStub.calledOnceWith(key);
     });
   });
